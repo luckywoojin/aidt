8c6c354cbed659b104b000d54cea4dcc0773dead
EATSTAR <53246103+luckywoojin@users.noreply.github.com>
2024-10-21 14:01:05 +0900

    Update 04_4차시.py

--- pages/04_4차시.py
+++ pages/04_4차시.py
@@ -25,74 +25,73 @@ if 'current_user' in st.session_state:
     if user_info:
         email, user_type = user_info  # 튜플에서 이메일과 사용자 유형 추출
         st.success(f'로그인한 사용자: {st.session_state["current_user"]}, 이메일: {email}, 사용자 유형: {user_type}')
-    else:
-        st.warning('로그인 상태가 아닙니다.')
-else:
-    st.warning('로그인 상태가 아닙니다.')
 
-st.subheader('4차시: 분석을 기반으로한 보고서 작성, 인식제고영상 보기')
 
-t1, t2, t3, t4, t5, t6 = st.tabs(['복습, 질문', '학습목표', '워드클라우드와 핵심 단어 데이터셋 비교', '인식제고영상', '학습정리', '선생님탭'])
+if st.session_state['login_status']:
+    st.subheader('4차시: 분석을 기반으로한 보고서 작성, 인식제고영상 보기')
 
-with t1:
-    st.success('서브1입니다.')
-    c1, c2 = st.columns((7, 3))
-    with c1:
-        st.write('빈페이지')
-    with c2:
-        st.write('빈페이지')
+    t1, t2, t3, t4, t5, t6 = st.tabs(['복습, 질문', '학습목표', '워드클라우드와 핵심 단어 데이터셋 비교', '인식제고영상', '학습정리', '선생님탭'])
 
-with t2:
-    st.success('서브2입니다.')
-    with st.expander('학습목표'):
-            st.subheader('오늘은 이러한 것을 배워봅시다.')
-            txtdata = '''
-    학습목표: 
-1. <br>
-2. <br>
-3.
-    '''
-            st.markdown(txtdata, unsafe_allow_html=True)
+    with t1:
+        st.success('서브1입니다.')
+        c1, c2 = st.columns((7, 3))
+        with c1:
+            st.write('빈페이지')
+        with c2:
+            st.write('빈페이지')
 
-with t3:
-    st.success('서브3입니다.')
-    st.write('빈페이지')
-
-with t4:
-    st.success('서브4입니다.')
-    c1, c2 = st.columns((7, 3))
-    with c1:
-        url = 'https://youtu.be/qyIbtz-l6q8?si=RMZ9ZJbBdqEdcvmQ'
-        st.video(url)
-    with c2:
-       st.write('미정') 
-    
-
-with t5:
-    st.success('서브5입니다.')
-    c1, c2 = st.columns((7, 3))
-    with c1:
-        with st.expander('오늘의 학습을 정리해봅시다.'):
-            st.subheader('오늘은 이러한 활동을 했습니다.')
-            txtdata = '''
+    with t2:
+        st.success('서브2입니다.')
+        with st.expander('학습목표'):
+                st.subheader('오늘은 이러한 것을 배워봅시다.')
+                txtdata = '''
+        학습목표: 
     1. <br>
     2. <br>
     3.
-    '''
-            st.markdown(txtdata, unsafe_allow_html=True)
+        '''
+                st.markdown(txtdata, unsafe_allow_html=True)
 
-        with st.expander('다음에는 어떤 활동을 할까요?'):
-            st.subheader('다음에는 이러한 활동을 할 것입니다.')
-            txtdata = '''
-    1. <br>
-    2. <br>
-    '''
-            st.markdown(txtdata, unsafe_allow_html=True)
-    with c2:
-        st.write('미정')
-
-with t6:
-    if st.session_state['login_status'] and st.session_state['current_user'] == 'admin':
+    with t3:
+        st.success('서브3입니다.')
         st.write('빈페이지')
-    else:
-        st.error('접근권한이 없습니다.')
+
+    with t4:
+        st.success('서브4입니다.')
+        c1, c2 = st.columns((7, 3))
+        with c1:
+            url = 'https://youtu.be/qyIbtz-l6q8?si=RMZ9ZJbBdqEdcvmQ'
+            st.video(url)
+        with c2:
+            st.write('미정') 
+        
+    with t5:
+        st.success('서브5입니다.')
+        c1, c2 = st.columns((7, 3))
+        with c1:
+            with st.expander('오늘의 학습을 정리해봅시다.'):
+                st.subheader('오늘은 이러한 활동을 했습니다.')
+                txtdata = '''
+        1. <br>
+        2. <br>
+        3.
+        '''
+                st.markdown(txtdata, unsafe_allow_html=True)
+
+            with st.expander('다음에는 어떤 활동을 할까요?'):
+                st.subheader('다음에는 이러한 활동을 할 것입니다.')
+                txtdata = '''
+        1. <br>
+        2. <br>
+        '''
+                st.markdown(txtdata, unsafe_allow_html=True)
+        with c2:
+            st.write('미정')
+
+    with t6:
+        if st.session_state['login_status'] and st.session_state['current_user'] == 'admin':
+            st.write('빈페이지')
+        else:
+            st.error('접근권한이 없습니다.')
+else:
+    st.error("로그인을 먼저하세요.")
