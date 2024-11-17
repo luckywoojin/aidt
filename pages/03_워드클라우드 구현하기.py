7a06b8b25d7213b9fcb6702c23e6f4c54795953f
EATSTAR <53246103+luckywoojin@users.noreply.github.com>
2024-11-17 21:11:35 +0900

    Update 03_워드클라우드 구현하기.py

--- pages/03_워드클라우드 구현하기.py
+++ pages/03_워드클라우드 구현하기.py
@@ -11,8 +11,8 @@ conn = sqlite3.connect('users.db')
 conn_user = conn.cursor()
 
 # question31.db 데이터베이스
-conn_2 = sqlite3.connect('question31.db')
-conn_question1 = conn_2.cursor()
+conn_1 = sqlite3.connect('question31.db')
+conn_question1 = conn_1.cursor()
 conn_question1.execute('''
     CREATE TABLE  IF NOT EXISTS question31 (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -20,11 +20,11 @@ conn_question1.execute('''
         txtfile TEXT NOT NULL,
         timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
     )
-''') # 만약 question31.db 파일이 존재하지 않는다면 해당 형식으로 생성함.
+''')
 
 # question32.db 데이터베이스 - 사실 tab3에는 존재하지 않으나 추후 추가할 것을 대비.
-conn_3 = sqlite3.connect('question32.db')
-conn_question2 = conn_3.cursor()
+conn_2 = sqlite3.connect('question32.db')
+conn_question2 = conn_2.cursor()
 conn_question2.execute('''
     CREATE TABLE  IF NOT EXISTS question32 (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -34,6 +34,29 @@ conn_question2.execute('''
     )
 ''')
 
+# question33.db 데이터베이스
+conn_3 = sqlite3.connect('question33.db')
+conn_question3 = conn_3.cursor()
+conn_question3.execute('''
+    CREATE TABLE  IF NOT EXISTS question33 (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        userid TEXT NOT NULL,
+        txtfile TEXT NOT NULL,
+        timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+    )
+''')
+
+def add_question3(txtfile):
+    userid = st.session_state.get('current_user')
+    if not userid:
+        st.error('비로그인 상태이므로 질문을 저장할 수 없습니다.')
+        return
+    else:
+        # Insert without the timestamp column
+        conn_question3.execute('INSERT INTO question33 (userid, txtfile) VALUES (?, ?)', (userid, txtfile))
+        conn_3.commit()
+        st.success('질문이 성공적으로 저장되었습니다!')
+
 def add_question2(txtfile):
     userid = st.session_state.get('current_user')
     if not userid:
@@ -42,7 +65,7 @@ def add_question2(txtfile):
     else:
         # Insert without the timestamp column
         conn_question2.execute('INSERT INTO question32 (userid, txtfile) VALUES (?, ?)', (userid, txtfile))
-        conn_3.commit()
+        conn_2.commit()
         st.success('질문이 성공적으로 저장되었습니다!')
 
 def add_question(txtfile):
@@ -53,7 +76,7 @@ def add_question(txtfile):
     else:
         # Insert without the timestamp column
         conn_question1.execute('INSERT INTO question31 (userid, txtfile) VALUES (?, ?)', (userid, txtfile))
-        conn_2.commit()  # Commit the transaction using conn_2, the connection to question31.db
+        conn_1.commit()  # Commit the transaction using conn_2, the connection to question31.db
         st.success('질문이 성공적으로 저장되었습니다!')
 
 # SQLite 데이터베이스 연결 (파일명: uploaded_files.db)
@@ -105,93 +128,15 @@ if st.session_state['login_status']:
 
     if selected_tab == '복습, 질문':
         log_record(3,1)
-        st.markdown("---")
-        st.subheader('접속 링크: https://aidtapp.streamlit.app/')
-        st.subheader('아이디/비번 : student / 123456')
-        st.markdown("---")
         st.subheader("전차시 복습")
-        
-    elif selected_tab == '학습목표':
-        log_record(3,2)
-        st.subheader("학습목표")
-
-    elif selected_tab == '워드클라우드란':
-        log_record(3,3)
-        st.subheader("워드 클라우드란?")
-
-        import os
-        # Streamlit 폴더 내의 이미지 파일 경로를 리스트로 준비
-        image_folder = "./images"  # 이미지 폴더 경로 설정
-        image_files = [os.path.join(image_folder, f) for f in os.listdir(image_folder) if f.endswith(('png', 'jpg', 'jpeg'))]
-
-        # 이미지가 4개 이상인지 확인하고 4개만 선택
-        image_files = image_files[:6] if len(image_files) >= 6 else image_files
-
-        # 이미지가 4개일 경우에만 2개씩 2줄로 표시
-        if len(image_files) == 6:
-            # 첫 번째 줄에 이미지 2개 표시
-            cols1 = st.columns(2)
-            cols1[0].image(image_files[0], caption="001", use_column_width=True)
-            cols1[1].image(image_files[1], caption="002", use_column_width=True)
-            
-            # 두 번째 줄에 이미지 2개 표시
-            cols2 = st.columns(2)
-            cols2[0].image(image_files[2], caption="003", use_column_width=True)
-            cols2[1].image(image_files[3], caption="004", use_column_width=True)
-
-            cols3 = st.columns(2)
-            cols3[0].image(image_files[4], caption="005", use_column_width=True)
-            cols3[1].image(image_files[5], caption="006", use_column_width=True)
-        else:
-            st.warning("4개의 이미지가 필요합니다. 이미지 파일 수를 확인해주세요.")
-    
-    elif selected_tab == '워드 클라우드 생성기':
-        log_record(3,4)
-        st.subheader("<실습 1> 워드 클라우드 생성기")
-
-        st.markdown("[팀 공유 폴더(이미지를 저장할 폴더)](https://drive.google.com/drive/folders/1eSEK2nEWM030_td1KuDUQiC1kOpm4cmP)", unsafe_allow_html=True)
-
-        import streamlit.components.v1 as components
-        url = 'https://wordcloud.kr/'
-        components.iframe(url, width=1024, height=1200)
-
-    elif selected_tab == '코랩으로 워드 클라우드 만들기':
-        log_record(3,4)
-        st.subheader("<실습 2> 코랩으로 워드 클라우드 만들기")
-
-
-        st.markdown("[코랩 워드 클라우드 코드](https://colab.research.google.com/drive/1lf80UP0VwzBOrxD7y2LwlKEPvWcoXYFd?usp=sharing)", unsafe_allow_html=True)
-        st.markdown("[팀 공유 폴더(이미지를 저장할 폴더)](https://drive.google.com/drive/folders/1eSEK2nEWM030_td1KuDUQiC1kOpm4cmP)", unsafe_allow_html=True)
-
-        import streamlit.components.v1 as components
-        url = 'https://wordcloud.kr/'
-        components.iframe(url, width=1024, height=1200)
-
-    elif selected_tab == '학습정리':
-        log_record(3,5)
         c1, c2 = st.columns((7, 3))
         with c1:
-            with st.expander('오늘의 학습을 정리해봅시다.'):
-                st.subheader('오늘은 이러한 활동을 했습니다.')
-                txtdata = '''
-        1. 이번 시간에는 파이썬을 통해 워드 클라우드 시각화를 진행했습니다.<br>
-        2. 이러한 시각화를 통해 평소 장애학생에게 주로 사용했던 용어에 대해<br>인식하는 계기가 되었습니다.<br>
-        '''
-                st.markdown(txtdata, unsafe_allow_html=True)
-
-            with st.expander('다음에는 어떤 활동을 할까요?'):
-                st.subheader('다음에는 이러한 활동을 할 것입니다.')
-                txtdata = '''
-        1. 다음 차시에는 오늘까지 완성해온 자료를 토대로 보고서를 작성할 것입니다.<br>
-        2. 또한, 장애인식제고 영상을 시청하여 보고서를 보완하겠습니다.<br>
-        3. 장애학생이 생활 속에서 얻는 어려움에 대해 알아보도록 하겠습니다.<br>
-        4. 과제를 구글 클래스룸에 제출하십시오.
-        '''
-                st.markdown(txtdata, unsafe_allow_html=True)
+            st.image('./images/01_studied_thing.png')
+            st.image('./images/01_studied_thing_answer.png')
         with c2:
             # 공부할거 같은 내용을 답변    
             with st.form('mynoteform'):
-                txtString = st.text_area('수업을 마치며 소감..', height=200)
+                txtString = st.text_area('어떤 것을 학습할 것 같나요?', height=200)
                 if st.form_submit_button('저장하기'): 
                     if not st.session_state['login_status']:
                         st.error('비로그인 상태이므로 메모를 저장할 수 없습니다.')
@@ -213,6 +158,108 @@ if st.session_state['login_status']:
                     questions11 = conn_question1.fetchall()
                     if questions11:
                         df_questions = pd.DataFrame(questions11, columns=["ID", "Question", "Timestamp"])
+                        st.dataframe(df_questions)
+                    else:
+                        st.warning("현재 저장된 질문이 없습니다.")
+
+    elif selected_tab == '학습목표':
+        log_record(3,2)
+        st.subheader("학습목표")
+        c1, c2 = st.columns((7, 3))
+        with c1:
+            st.image('./images/02_learning_index.png')
+
+    elif selected_tab == '워드클라우드란':
+        log_record(3,3)
+        st.subheader("워드 클라우드란?")
+        c1, c2 = st.columns((7, 3))
+        with c1:
+            st.image('./images/03_wordcloud1.png')
+            st.image('./images/03_wordcloud2.png')
+        with c2:
+            # 워드 클라우드에 관한 내용을 메모하도록 유도    
+            with st.form('mynoteform'):
+                txtString = st.text_area('학습 메모', height=200)
+                if st.form_submit_button('저장하기'): 
+                    if not st.session_state['login_status']:
+                        st.error('비로그인 상태이므로 메모를 저장할 수 없습니다.')
+                    else:
+                        if txtString != '':
+                            add_question2(txtString)
+
+                        else:
+                            st.error('노트가 비어 있어요 ㅠㅠㅠ')  
+
+            # 질문 2의 자신의 답변에 대해 볼 수 있게 하는 기능
+            if st.session_state['login_status'] and 'current_user' in st.session_state:
+                with st.expander('자신의 답변 보기'):
+                    user_id = st.session_state['current_user']
+                    
+                    # Display only user-specific questions for question1
+                    st.write("About: 질문 탭")
+                    conn_question2.execute('SELECT id, txtfile, timestamp FROM question32 WHERE userid = ? ORDER BY timestamp DESC', (user_id,))
+                    questions22 = conn_question2.fetchall()
+                    if questions22:
+                        df_questions = pd.DataFrame(questions22, columns=["ID", "Question", "Timestamp"])
+                        st.dataframe(df_questions)
+                    else:
+                        st.warning("현재 저장된 질문이 없습니다.")
+    
+    elif selected_tab == '워드 클라우드 생성기':
+        log_record(3,4)
+        st.subheader("<실습 1> 워드 클라우드 생성기")
+        st.markdown("[구글 드라이브 링크 (팀 공유 폴더)](https://drive.google.com/drive/folders/1eSEK2nEWM030_td1KuDUQiC1kOpm4cmP)", unsafe_allow_html=True)
+        c1, c2 = st.columns((7, 3))
+        with c1:
+            st.image('./images/04_generator1.png')
+            st.image('./images/04_generator2.png')
+
+        import streamlit.components.v1 as components
+        url = 'https://wordcloud.kr/'
+        components.iframe(url, width=1024, height=1200)
+
+    elif selected_tab == '코랩으로 워드 클라우드 만들기':
+        log_record(3,4)
+        st.subheader("<실습 2> 코랩으로 워드 클라우드 만들기")
+        st.markdown("[구글 드라이브 링크 (팀 공유 폴더)](https://drive.google.com/drive/folders/1eSEK2nEWM030_td1KuDUQiC1kOpm4cmP)", unsafe_allow_html=True)
+        c1, c2 = st.columns((7, 3))
+        with c1:
+            st.image('./images/05_python1.png')
+            st.image('./images/05_python2.png')
+            st.image('./images/05_python3.png')
+
+    elif selected_tab == '학습정리':
+        log_record(3,5)
+        c1, c2 = st.columns((7, 3))
+        with c1:
+            st.image('./images/06_final1.png')
+            st.image('./images/06_final2.png')
+
+        with c2:
+            # 공부할거 같은 내용을 답변    
+            with st.form('mynoteform'):
+                txtString = st.text_area('수업을 마치며 소감..', height=200)
+                if st.form_submit_button('저장하기'): 
+                    if not st.session_state['login_status']:
+                        st.error('비로그인 상태이므로 메모를 저장할 수 없습니다.')
+                    else:
+                        if txtString != '':
+                            add_question3(txtString)
+
+                        else:
+                            st.error('노트가 비어 있어요 ㅠㅠㅠ')  
+
+            # 질문 3의 자신의 답변에 대해 볼 수 있게 하는 기능
+            if st.session_state['login_status'] and 'current_user' in st.session_state:
+                with st.expander('자신의 답변 보기'):
+                    user_id = st.session_state['current_user']
+                    
+                    # Display only user-specific questions for question1
+                    st.write("About: 질문 탭")
+                    conn_question3.execute('SELECT id, txtfile, timestamp FROM question33 WHERE userid = ? ORDER BY timestamp DESC', (user_id,))
+                    questions33 = conn_question3.fetchall()
+                    if questions33:
+                        df_questions = pd.DataFrame(questions33, columns=["ID", "Question", "Timestamp"])
                         st.dataframe(df_questions)
                     else:
                         st.warning("현재 저장된 질문이 없습니다.")
